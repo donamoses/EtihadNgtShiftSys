e47825ab9c377eebbd194f88f36aae3fa0e96b3a
Geethu Krishna <geethu.krishna@ccsdev01.onmicrosoft.com>
2024-08-07 08:30:51 +0530

    Initial Commit

---
+++ src/webparts/etihadManReqForm/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IEtihadManReqFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'EtihadManReqFormWebPartStrings' {
+  const strings: IEtihadManReqFormWebPartStrings;
+  export = strings;
+}
---
+++ src/webparts/etihadViewReqDashboard/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IEtihadViewReqDashboardWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'EtihadViewReqDashboardWebPartStrings' {
+  const strings: IEtihadViewReqDashboardWebPartStrings;
+  export = strings;
+}
